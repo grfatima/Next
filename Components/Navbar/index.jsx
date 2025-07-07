884873edeb19cb4fdc6e6e3a6274f009b2c60518
grfatime <kurbanovhafatime@gmail.com>
2025-07-07 15:04:19 +0400

    next

---
+++ Components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className=" bg-pink-300 text-white flex justify-evenly h-20 items-center text-2xl">
+      <Link href="/">Author</Link>
+      <Link href="/genre">Genre</Link>
+      <Link href="/book">Book</Link>
+    </div>
+  );
+};
+
+export default Navbar;
